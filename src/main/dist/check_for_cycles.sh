ec1e37cb3bddb7d3f640d74b0f6f7fd0759d4989
tutajm <mtutaj@mcw.edu>
2019-09-20 16:34:05 -0500

    fix typo

--- src/main/dist/check_for_cycles.sh
+++ src/main/dist/check_for_cycles.sh
@@ -4,9 +4,9 @@
 APPDIR=/home/rgddata/pipelines/OntologyLoad
 cd $APPDIR
 
-if [ "%1" == "" ]; then
+if [ "$1" == "" ]; then
   echo "Please enter a parameter for ontology id, for example RDO"
   exit -1
 fi
 
-$APPDIR/_run.sh -checkForCycles -single_ontology=%1 -skip_downloads -skip_stats_update
+$APPDIR/_run.sh -checkForCycles -single_ontology=$1 -skip_downloads -skip_stats_update
