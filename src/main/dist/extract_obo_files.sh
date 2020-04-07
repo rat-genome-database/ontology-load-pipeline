4cf585e54d816ef9c55ef1cd6a3981d52c517bb4
tutajm <mtutaj@mcw.edu>
2020-04-07 13:49:15 -0500

    obo-generator: improved summary email

--- src/main/dist/extract_obo_files.sh
+++ src/main/dist/extract_obo_files.sh
@@ -10,46 +10,8 @@ fi
 
 cd $APPDIR
 
-# PATHWAY
-$APPDIR/_run.sh -generate_obo_file=PW -process_obsolete_terms -prod_release
+# generate obo files for all ontologies specified in config file AppConfigure.xml
+$APPDIR/_run.sh -generate_obo_file=
 
-scp -p $APPDIR/data/ontology/pathway/*.obo $ONT_RELEASE_DIR/pathway
-echo "staging dir: $ONT_RELEASE_DIR/pathway"
-echo
-
-# CLINICAL MEASUREMENT
-$APPDIR/_run.sh -generate_obo_file=CMO -process_obsolete_terms -prod_release
-
-scp -p $APPDIR/data/ontology/clinical_measurement/*.obo $ONT_RELEASE_DIR/clinical_measurement
-echo "staging dir: $ONT_RELEASE_DIR/clinical_measurement"
-echo
-
-# EXPERIMENTAL CONDITION
-$APPDIR/_run.sh -generate_obo_file=XCO -process_obsolete_terms -prod_release
-
-scp -p $APPDIR/data/ontology/experimental_condition/*.obo $ONT_RELEASE_DIR/experimental_condition
-echo "staging dir: $ONT_RELEASE_DIR/experimental_condition"
-echo
-
-# MEASUREMENT METHOD
-$APPDIR/_run.sh -generate_obo_file=MMO -process_obsolete_terms -prod_release
-
-scp -p $APPDIR/data/ontology/measurement_method/*.obo $ONT_RELEASE_DIR/measurement_method
-echo "staging dir: $ONT_RELEASE_DIR/measurement_method"
-echo
-
-
-# RDO
-$APPDIR/_run.sh -generate_obo_file=RDO -prod_release
-
-scp -p $APPDIR/data/ontology/disease/*.obo $ONT_RELEASE_DIR/disease
-echo "staging dir: $ONT_RELEASE_DIR/disease"
-echo
-
-
-# RS
-$APPDIR/_run.sh -generate_obo_file=RS -prod_release
-
-scp -p $APPDIR/data/ontology/rat_strain/*.obo $ONT_RELEASE_DIR/rat_strain
-echo "staging dir: $ONT_RELEASE_DIR/rat_strain"
-echo
+# copy all the files to the data release directory for ontologies
+scp -pr $APPDIR/data/ontology/* $ONT_RELEASE_DIR
