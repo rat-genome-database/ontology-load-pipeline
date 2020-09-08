ef7fb3b1ab41a857daaa0c3d9fa135075f581157
De Pons <jdepons@rgd-dep-mac.local>
2020-09-08 15:13:54 -0500

    added hp

--- src/main/dist/run_portal.sh
+++ src/main/dist/run_portal.sh
@@ -3,7 +3,7 @@ APP=/home/rgddata/pipelines/OntologyLoad
 
 #ontologies=( "CHEBI" )
 #ontologies=( "MP" )
-ontologies=( "XCO" "CMO" "PW" "VT" "RS" "MP" "RDO" "GO" "CHEBI" )
+ontologies=( "XCO" "CMO" "PW" "VT" "RS" "MP" "HP" "RDO" "GO" "CHEBI" )
 #ontologies=( "RDO" "XCO" "PW" "CMO" "VT" "RS" "MP" "GO" )
 
 for ontology in "${ontologies[@]}"
