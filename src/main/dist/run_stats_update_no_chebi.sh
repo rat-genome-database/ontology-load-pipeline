79b8a5749b8b7be26da1c50145499464158d53fc
tutajm <mtutaj@mcw.edu>
2023-05-08 09:48:22 -0500

    tuned up script run_stats_update_no_chebi.sh

--- src/main/dist/run_stats_update_no_chebi.sh
+++ src/main/dist/run_stats_update_no_chebi.sh
@@ -6,8 +6,15 @@ cd $APPDIR
 
 ontologies=( "RDO" "GO" "MP" "HP" "MMO" "CMO" "XCO" "RS" "PW" )
 
+LOGFILE=$APPDIR/stats_update_no_chebi.log
+echo "" > $LOGFILE
+
 for ontology in "${ontologies[@]}"; do
     #update ontology term stats
-    $APPDIR/run_single.sh "$ontology" -skip_downloads
-    echo ""
+    $APPDIR/run_single.sh "$ontology" -skip_downloads >> $LOGFILE
 done
+
+echo "===" >> $LOGFILE
+echo "DONE!" >> $LOGFILE
+
+mailx -s "[$SERVER] Update Ontology term stats, no CHEBI" mtutaj@mcw.edu < $LOGFILE
