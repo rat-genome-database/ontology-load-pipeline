a5be770fdacd24ca174058f56254eb3fd64a0bd6
tutajm <mtutaj@mcw.edu>
2023-11-07 14:02:14 -0600

    updated EEfoBulkLoad: EFO-CMO

--- src/main/java/edu/mcw/rgd/dataload/ontologies/test/EfoBulkLoad.java
+++ src/main/java/edu/mcw/rgd/dataload/ontologies/test/EfoBulkLoad.java
@@ -19,6 +19,7 @@ public class EfoBulkLoad {
         fname = "/Users/mtutaj/Documents/EFO-RDO-bulk-load.txt";
         fname = "/tmp/EFO to HP-MP automapping bulk load.txt";
         fname = "/tmp/EFO-HP-MP list for bulk load.txt";
+        fname = "/tmp/EFO-CMO mappings bulk load 2.txt";
         BufferedReader in = Utils.openReader(fname);
 
         int linesWithIssues = 0;
@@ -27,50 +28,51 @@ public class EfoBulkLoad {
         int synonymsInserted = 0;
         int synonymsUpToDate = 0;
 
-        //old: EFO:0004720	MP:0002654	prion disease	broad_synonym
-        //new: EFO:0000771	bacterial disease	Opportunistic bacterial infection	HP:0032260	broad_synonym
+        //example
+        // EFO ID as xref | EFO term as synonym | synonym type | CMO term | CMO ID
+        // EFO:0004295	left ventricular function	broad_synonym	time constant of left ventricular pressure decay	CMO:0001747
         String line;
         int lineNr = 0;
         while( (line=in.readLine())!=null ) {
             lineNr++;
             String[] cols = line.split("[\\t]", -1);
             if( cols.length<5 ) {
-                System.out.println(lineNr+". line skipped: "+line);
+                System.out.println(lineNr+". ### line skipped: "+line);
                 linesWithIssues++;
                 continue;
             }
             String xrefAcc = getText(cols[0]);
-            String hpMpAcc = getText(cols[3]);
+            String cmoAcc = getText(cols[4]);
             String synonymName = getText(cols[1]); // EFO term name will be the synonym name
             if( synonymName.startsWith("\"") && synonymName.endsWith("\"") ) {
                 synonymName = getText(synonymName.substring(1, synonymName.length()-1));
             }
-            String synonymType = getText(cols[4]);
+            String synonymType = getText(cols[2]);
 
             if( !xrefAcc.startsWith("EFO:") && !xrefAcc.startsWith("MONDO:") ) {
-                System.out.println(lineNr+". not found EFO/MONDO acc in line "+line);
+                System.out.println(lineNr+". ### not found EFO/MONDO acc in line "+line);
                 linesWithIssues++;
                 continue;
             }
-            if( !hpMpAcc.startsWith("HP:") && !hpMpAcc.startsWith("MP:")) {
-                System.out.println(lineNr+". not found HP/MP acc in line "+line);
+            if( !cmoAcc.startsWith("CMO:")) {
+                System.out.println(lineNr+". ### not found CMO acc in line "+line);
                 linesWithIssues++;
                 continue;
             }
 
 
-            Term term = dao.getTerm(hpMpAcc);
+            Term term = dao.getTerm(cmoAcc);
             if( term==null ) {
-                System.out.println(lineNr+". "+hpMpAcc+" not found in RGD!");
+                System.out.println(lineNr+". ### "+cmoAcc+" not found in RGD!");
                 linesWithIssues++;
                 continue;
             }
 
             boolean xrefAlreadyInRgd = false;
-            List<TermSynonym> synonyms = dao.getTermSynonyms(hpMpAcc);
+            List<TermSynonym> synonyms = dao.getTermSynonyms(cmoAcc);
             for( TermSynonym syn: synonyms ) {
                 if( syn.getName().equals(xrefAcc) ) {
-                    System.out.println(lineNr+". "+hpMpAcc+" "+xrefAcc+" already in RGD!");
+                    System.out.println(lineNr+". "+cmoAcc+" "+xrefAcc+" already in RGD!");
                     xrefAlreadyInRgd = true;
                     break;
                 }
@@ -81,9 +83,9 @@ public class EfoBulkLoad {
                 TermSynonym syn = new TermSynonym();
                 syn.setType("xref");
                 syn.setName(xrefAcc);
-                syn.setTermAcc(hpMpAcc);
+                syn.setTermAcc(cmoAcc);
                 dao.insertTermSynonym(syn, "BULKLOAD");
-                System.out.println(lineNr + ". inserted " + hpMpAcc + " " + xrefAcc);
+                System.out.println(lineNr + ". inserted " + cmoAcc + " " + xrefAcc);
                 xrefsInserted++;
             }
 
@@ -92,7 +94,7 @@ public class EfoBulkLoad {
                 boolean synonymAlreadyInRgd = false;
                 for( TermSynonym s: synonyms ) {
                     if( s.getName().equalsIgnoreCase(synonymName) ) {
-                        System.out.println(lineNr+". "+hpMpAcc+" ["+synonymName+"] already in RGD!");
+                        System.out.println(lineNr+". "+cmoAcc+" ["+synonymName+"] already in RGD!");
                         synonymAlreadyInRgd = true;
                         break;
                     }
@@ -104,9 +106,9 @@ public class EfoBulkLoad {
                     TermSynonym syn = new TermSynonym();
                     syn.setType(synonymType);
                     syn.setName(synonymName);
-                    syn.setTermAcc(hpMpAcc);
+                    syn.setTermAcc(cmoAcc);
                     dao.insertTermSynonym(syn, "BULKLOAD");
-                    System.out.println(lineNr + ". inserted " + hpMpAcc + " [" + synonymName + "] (" + synonymType + ")");
+                    System.out.println(lineNr + ". inserted " + cmoAcc + " [" + synonymName + "] (" + synonymType + ")");
                     synonymsInserted++;
                 }
             }
