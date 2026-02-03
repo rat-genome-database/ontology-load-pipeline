6b23db137f2d0b15502aa51bbe4b32ba062c4d92
tutajm <mtutaj@mcw.edu>
2026-02-03 10:04:53 -0600

    check_for_cycles.sh: added usage info

--- src/main/dist/check_for_cycles.sh
+++ src/main/dist/check_for_cycles.sh
@@ -6,6 +6,11 @@ cd $APPDIR
 
 if [ "$1" == "" ]; then
   echo "Please enter a parameter for ontology id, for example RDO, or * for all public ontologies"
+  echo ""
+  echo "  example 1  -- run check for cycles for EFO ontology"
+  echo "  ./check_for_cycles.sh EFO"
+  echo "  example 2  -- run check for cycles for all public ontologies"
+  echo "  ./check_for_cycles.sh '*'"
   exit -1
 fi
 
